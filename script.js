fbfcd852b3162893a1ef59493e41fab14090c878
Margaret Seiche <margaretseiche@hotmail.com>
2020-06-11 20:27:05 -0400

    fixed colors and cleaned up code

--- script.js
+++ script.js
@@ -7,8 +7,7 @@ $(document).ready(function() {
     currentTime.text(moment().format('h:mm a'));
 
     var hourArray = ["8:00 am","9:00 am","10:00 am","11:00 am","12:00 pm","1:00 pm","2:00 pm","3:00 pm","4:00 pm","5:00 pm","6:00 pm","7:00 pm"]; 
-    var hourArray2 = [12,13,14,15,16,17,18,19,20,21,22,23]; 
-    var hourArray3 = [8,9,10,11,12,13,14,15,16,17,18,19]; 
+    var hourArray2 = [8,9,10,11,12,13,14,15,16,17,18,19]; 
     
     var todos = ["","","","","test","","","","","",""];
 
@@ -20,44 +19,44 @@ $(document).ready(function() {
 
     function renderTodos() {        
         for (var i = 0; i < hourArray.length; i++) {
-            hourBlock = $("<div>");
-                $(hourBlock).attr("class","row");
-                $(container).append(hourBlock);
+            hourBlock = $("<div>")
+                .attr("class","row")
+                .appendTo(container);
 
-            hour = $("<div>");
-                $(hour).text(hourArray[i]);
-                $(hour).attr("class","hour col-2");
-                $(hourBlock).append(hour);
+            hour = $("<div>")
+                .text(hourArray[i])
+                .attr("class","hour col-2")
+                .appendTo(hourBlock);
 
-            description = $("<div>");
-                $(description).text("");
-                $(description).attr("class","description col-8");
-                $(description).attr("id","description");
-                $(hourBlock).append(description);
+            description = $("<div>")
+                .text("")
+                .attr("class","description col-8")
+                .attr("id","description-" + i)
+                .appendTo(hourBlock);
 
-            textarea = $("<textarea>");
-                $(textarea).text(todos[i]);
-                $(textarea).attr("class","textarea col-12");
-                $(textarea).attr("type","text");
-                $(description).append(textarea);
+            textarea = $("<textarea>")
+                .text(todos[i])
+                .attr("class","textarea col-12")
+                .attr("type","text")
+                .appendTo(description);
 
-            saveBtn = $("<button>");
-                $(saveBtn).text("Save");  
-                $(saveBtn).attr("class","saveBtn col-2");
-                $(hourBlock).append(saveBtn);   
+            saveBtn = $("<button>")
+                .text("Save")  
+                .attr("class","saveBtn col-2")
+                .appendTo(hourBlock);   
 
-            saveImage = $("<img>");
-                $(saveImage).attr("src","./saveicon.png");
-                $(saveImage).css({height:"50px",width:"60px", paddingLeft:"15px"});
-                $(saveBtn).append(saveImage);   
+            saveImage = $("<img>")
+                .attr("src","./saveicon.png")
+                .css({height:"50px",width:"60px", paddingLeft:"15px"})
+                .appendTo(saveBtn);   
             
             $(".saveBtn").on("click", function() {
                 todos.splice(i, 1, textarea[i]);
 
-                bellImage = $("<img>");
-                $(bellImage).attr("src","./bell.jpg");
-                $(bellImage).css({height:"50px",width:"50px", paddingLeft:"15px"});
-                $(hour).append(bellImage);   
+                bellImage = $("<img>")
+                .attr("src","./bell.jpg")
+                .css({height:"50px",width:"50px", paddingLeft:"15px"})
+                .appendTo(hour);   
             });   
         };        
     }
@@ -66,35 +65,19 @@ $(document).ready(function() {
         for (var j = 0; j < hourArray2.length; j++) {
            
             var currentHour = Number(moment().startOf('day').fromNow().slice(0,2).trim());
-
-        /*    console.log(currentHour);        */
-            console.log(hourArray2[j]);
-
-    /*        if (currentHour < hourArray2[j]) { 
-                console.log("future");               
-                 $("#description").attr("id","future description");
-             } else if (currentHour == hourArray2[j]) {
-                 console.log("present");
-                 $("#description").attr("id","present description");
-             } else if (currentHour > hourArray2[j]) {
-                 $("#description").attr("id","past description");  
-                 console.log("past");                   */
                  
-                 if (currentHour < hourArray2[j]) { 
-                    console.log("future");               
-                     $(".description").css({
+                if (currentHour < hourArray2[j]) { 
+                     $(`#description-${j}`).css({
                         backgroundColor: "#77dd77",
                         color: "white"
                           });
                  } else if (currentHour == hourArray2[j]) {
-                     console.log("present");
-                     $(".description").css({
+                     $(`#description-${j}`).css({ 
                         backgroundColor: "#ff6961",
                         color: "white"
                      });
                  } else if (currentHour > hourArray2[j]) {
-                    console.log("past");    
-                    $(".description").css({
+                    $(`#description-${j}`).css({
                         backgroundColor: "#d3d3d3",
                         color: "white"
                 });
